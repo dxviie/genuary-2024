0a34ebdde5021471a75c6957607d426cd9a46c8e
david <david@d17e.dev>
2024-01-02 18:49:49 +0100

    reseed button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -25,6 +25,13 @@
         key++;
     }
 
+    function handleDice() {
+        if (selectedSketch.reset) {
+            selectedSketch.reset();
+        }
+        key++;
+    }
+
 </script>
 
 <main>
@@ -40,6 +47,12 @@
                 </option>
             {/each}
         </select>
+
+        <button class="dice-button" on:click={handleDice}>
+            <svg class="dice-svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M8 8H8.01M16 8H16.01M12 12H12.01M16 16H16.01M8 16H8.01M7.2 20H16.8C17.9201 20 18.4802 20 18.908 19.782C19.2843 19.5903 19.5903 19.2843 19.782 18.908C20 18.4802 20 17.9201 20 16.8V7.2C20 6.0799 20 5.51984 19.782 5.09202C19.5903 4.71569 19.2843 4.40973 18.908 4.21799C18.4802 4 17.9201 4 16.8 4H7.2C6.0799 4 5.51984 4 5.09202 4.21799C4.71569 4.40973 4.40973 4.71569 4.21799 5.09202C4 5.51984 4 6.07989 4 7.2V16.8C4 17.9201 4 18.4802 4.21799 18.908C4.40973 19.2843 4.71569 19.5903 5.09202 19.782C5.51984 20 6.07989 20 7.2 20Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+            </svg>
+        </button>
     </div>
 
     <PaperCanvas key={key}
@@ -104,6 +117,29 @@
         border-color: rgba(0, 0, 0, 1);
         border-width: 1px;
         border-style: dashed;
+    }
+
+    .dice-button {
+        border: none;
+        background-color: transparent;
+        cursor: pointer;
+        padding: 10px;
+    }
+
+    .dice-button:hover {
+        transform: scale(1.1);
+        transition: transform 0.1s ease-in-out;
+    }
+
+    .dice-button:active {
+        transform: translate(1px, 1px);
+        transition: transform 0.1s ease-in-out;
+    }
+
+    .dice-svg {
+        /*fill: #000;*/
+        width: 50px;
+        height: 50px;
     }
 
     .footer {
