b1100af16407bcf4990f1b7506d20ec1bcafabbe
david <david@d17e.dev>
2024-01-02 19:04:04 +0100

    accessibility

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,7 +6,7 @@
     let key = 0;
     let debug = false;
     let sketches = [
-        {name: "00. Select prompt.", sketch: () => {}, reset: () => {}},
+        {name: "00. No sketch selected.", sketch: () => {}, reset: () => {}},
         {name: "01. Particles. Lots of them.", sketch: drawParticles, reset: clearParticles, animation: true},
         {name: "02. No palettes.", sketch: drawGenerativeColors, reset: resetColors, animation: false}
     ];
@@ -40,7 +40,8 @@
     <h2>entries by <a href="https://d17e.dev" target="_self">d17e.dev</a></h2>
     <div class="header">
 
-        <select on:change={handleSelectSketch}>
+        <label for="sketch-selection" style="width: 14rem; margin-right: .5rem;">select sketch:</label>
+        <select on:change={handleSelectSketch} id="sketch-selection">
             {#each sketches as sketch, index}
                 <option value={index}>
                     {sketch.name}
@@ -48,7 +49,8 @@
             {/each}
         </select>
 
-        <button class="dice-button" on:click={handleDice}>
+        <div style="width: 3rem;"></div>
+        <button class="dice-button" on:click={handleDice} title="reset the sketch">
             <svg class="dice-svg" width="800px" height="800px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M8 8H8.01M16 8H16.01M12 12H12.01M16 16H16.01M8 16H8.01M7.2 20H16.8C17.9201 20 18.4802 20 18.908 19.782C19.2843 19.5903 19.5903 19.2843 19.782 18.908C20 18.4802 20 17.9201 20 16.8V7.2C20 6.0799 20 5.51984 19.782 5.09202C19.5903 4.71569 19.2843 4.40973 18.908 4.21799C18.4802 4 17.9201 4 16.8 4H7.2C6.0799 4 5.51984 4 5.09202 4.21799C4.71569 4.40973 4.40973 4.71569 4.21799 5.09202C4 5.51984 4 6.07989 4 7.2V16.8C4 17.9201 4 18.4802 4.21799 18.908C4.40973 19.2843 4.71569 19.5903 5.09202 19.782C5.51984 20 6.07989 20 7.2 20Z" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
             </svg>
