f37d81c9139f61510404cbe49a6694ade8fe0a33
david <david@d17e.dev>
2024-01-04 23:37:47 +0100

    04. Pixels!!

--- src/lib/utils/genuary.2024.04.js
+++ src/lib/utils/genuary.2024.04.js
@@ -1,15 +1,79 @@
+import {getRandomInt} from "$lib/utils/ToolBox.js";
+
 export function clearPixels() {
-    shapes.forEach((shape) => {
-        shape.remove();
-    });
-    shapes = [];
+    if (screen) {
+        screen.screen.remove();
+        screen.columns.forEach(column => column.remove());
+        screen = null;
+    }
+    if (tracer) {
+        tracer.forEach(block => block.remove());
+        tracer = null;
+    }
+    red = getRandomInt(30, 255);
+    green = getRandomInt(30, 255);
+    blue = getRandomInt(30, 255);
 }
 
-const PIXEL_DIM = 49;
-let shapes = [];
+const PIXEL_DIM = 27;
+let screen = null;
+let tracer = null;
+let red = 255;
+let green = 255;
+let blue = 255;
+
 export function drawPixels(paper, event, debug) {
     let canvasSize = paper.view.bounds.width * 0.9;
     let canvasOffset = (paper.view.bounds.width - canvasSize) / 2;
+    let pixelSize = canvasSize / PIXEL_DIM;
+    let shift = pixelSize / 4;
+
+    if (!screen) {
+        screen = createCRTScreen(paper, canvasOffset, pixelSize, shift);
+    }
+    if (!tracer) {
+        tracer = createTracer(paper, canvasOffset, canvasSize, pixelSize);
+    }
+
+    screen.pixels.forEach(pixel => {
+        pixel.group.opacity -= 0.1;
+        // pixel.group.strokeWidth = debug ? 1 : 0;
+    });
+
+    let amplitude = (canvasSize / 2) * 0.8;
+    let frequency = 6;
+    let t = (event && event.time) ? event.time : 0;
+
+    for (let i = 0; i < tracer.length; i++) {
+        let block = tracer[i];
+        let x = (((block.bounds.x + block.bounds.width / 2) - canvasOffset) / canvasSize) * frequency;
+        let y = Math.sin(x+t);
+
+        block.bounds.y = y * amplitude + canvasSize/2 + canvasOffset/2;// * amplitude + canvasOffset + canvasSize/2;
+        block.strokeWidth = debug ? 1 : 0;
+
+        let lower = Math.ceil(((block.bounds.y - canvasOffset) / canvasSize) * PIXEL_DIM);
+        let upper = Math.floor(((block.bounds.y + block.bounds.height - canvasOffset) / canvasSize) * PIXEL_DIM);
+        let pixels = screen.pixelColumns[i];
+        if (pixels[lower]) {
+            pixels[lower].setRed(red);
+            pixels[lower].setGreen(green);
+            pixels[lower].setBlue(blue);
+            pixels[lower].setGlow('rgb(red, green, blue)');
+            pixels[lower].group.opacity = 1;
+
+        }
+        if (pixels[upper]) {
+            pixels[upper].setRed(red);
+            pixels[upper].setGreen(green);
+            pixels[upper].setBlue(blue);
+            pixels[upper].setGlow('rgb(255, 255, 255)');
+            pixels[upper].group.opacity = 1;
+        }
+    }
+}
+
+function createCRTScreen(paper, canvasOffset, pixelSize, shift) {
 
     let background = new paper.Path.Rectangle({
         point: [0, 0],
@@ -18,29 +82,32 @@ export function drawPixels(paper, event, debug) {
         strokeColor: 'rgb(0, 0, 0)',
         strokeWidth: 1
     })
-    shapes.push(background);
 
-    let pixelSize = canvasSize / PIXEL_DIM;
-    let shift = pixelSize / 4;
-
-    const pixelColor = {
-        red: 1,
-        green: 1,
-        blue: 1
-    }
-
+    let columns = [];
+    let pixelColumns = [];
+    let pixels = [];
     for (let x = 0; x < PIXEL_DIM; x++) {
         const column = new paper.Group();
+        let pixelColumn = [];
         for (let y = 0; y < PIXEL_DIM; y++) {
-            let pixel = createCRTPixel(paper, canvasOffset + x * pixelSize, canvasOffset + y * pixelSize, pixelSize, pixelColor, debug);
-            column.addChild(pixel);
+            let pixel = createCRTPixel(paper, canvasOffset + x * pixelSize, canvasOffset + y * pixelSize, pixelSize);
+            column.addChild(pixel.group);
+            pixelColumn.push(pixel);
+            pixels.push(pixel);
         }
         column.translate(new paper.Point(0, x%2 === 0 ? shift : -shift));
-        shapes.push(column);
+        columns.push(column);
+        pixelColumns.push(pixelColumn);
+    }
+    return {
+        columns: columns,
+        pixelColumns: pixelColumns,
+        pixels: pixels,
+        screen: background
     }
 }
 
-function createCRTPixel(paper, x, y, size, color, debug) {
+function createCRTPixel(paper, x, y, size) {
     const width = size / 3 * 0.9;
     const padding = ((size / 3) * 0.1) / 2;
     // Create the red stripe
@@ -49,9 +116,9 @@ function createCRTPixel(paper, x, y, size, color, debug) {
         size: new paper.Size(width, size),
         radius: width/2,
         strokeColor: 'black',
-        strokeWidth: 0
+        strokeWidth: 0,
+        fillColor: 'rgb(0, 0, 0)'
     });
-    redStripe.fillColor = (color && color.red) ? new paper.Color(color.red/255, 0, 0) : 'rgb(255, 0, 0)';
 
     // Create the green stripe
     var greenStripe = new paper.Path.Rectangle({
@@ -59,9 +126,9 @@ function createCRTPixel(paper, x, y, size, color, debug) {
         size: new paper.Size(width, size),
         radius: width/2,
         strokeColor: 'black',
-        strokeWidth: 0
+        strokeWidth: 0,
+        fillColor: 'rgb(0, 0, 0)'
     });
-    greenStripe.fillColor = (color && color.green) ? new paper.Color(0, color.green/255, 0) : 'rgb(0, 255, 0)';
 
     // Create the blue stripe
     var blueStripe = new paper.Path.Rectangle({
@@ -69,22 +136,51 @@ function createCRTPixel(paper, x, y, size, color, debug) {
         size: new paper.Size(width, size),
         radius: width/2,
         strokeColor: 'black',
-        strokeWidth: 0
+        strokeWidth: 0,
+        fillColor: 'rgb(0, 0, 0)'
     });
-    blueStripe.fillColor = (color && color.blue) ? new paper.Color(0, 0, color.blue/255) : 'rgb(0, 0, 255)';
 
     let glow = new paper.Path.Rectangle({
         topLeft: new paper.Point(x, y),
         size: new paper.Size(size, size),
         radius: width/2,
-        fillColor: color,
+        fillColor: 'rgb(0, 0, 0)',
         strokeColor: 'rgb(255, 255, 255)',
-        strokeWidth: debug ? 1 : 0,
+        strokeWidth: 1,
         opacity: 0.3
     })
 
     // Group the stripes for easier manipulation
     var crtPixel = new paper.Group([redStripe, greenStripe, blueStripe, glow]);
     glow.sendToBack();
-    return crtPixel;
+    return {
+        group: crtPixel,
+        setRed: (value) => {
+            redStripe.fillColor = new paper.Color(value/255, 0, 0);
+        },
+        setGreen: (value) => {
+            greenStripe.fillColor = new paper.Color(0, value/255, 0);
+        },
+        setBlue: (value) => {
+            blueStripe.fillColor = new paper.Color(0, 0, value/255);
+        },
+        setGlow: (color) => {
+            glow.fillColor = color;
+        }
+
+    };
+}
+
+function createTracer(paper, canvasOffset, canvasSize, pixelSize) {
+    let tracer = [];
+    for (let i = 0; i < Math.floor(canvasSize / pixelSize); i++) {
+        let block = new paper.Path.Rectangle({
+            point: [canvasOffset + i * pixelSize, canvasOffset],
+            size: [pixelSize, pixelSize * 2],
+            strokeColor: 'rgb(255, 0, 0)',
+            strokeWidth: 1
+        })
+        tracer.push(block);
+    }
+    return tracer;
 }
