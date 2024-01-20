28b6458ce1489944f14dec6b269d50545a940b4f
david <david@d17e.dev>
2024-01-20 17:02:11 +0100

    05 image added WIP

--- src/lib/utils/genuary.2024.05.js
+++ src/lib/utils/genuary.2024.05.js
@@ -1,3 +1,34 @@
 export function clearVera() {}
 
-export function drawVera() {}
+let resolution = 39; //78;
+export function drawVera(paper, event, debug) {
+    if (!paper || !paper.project || !paper.project.activeLayer) {
+        return;
+    }
+    paper.project.activeLayer.removeChildren();
+
+    // image found & edited from https://www.holo.mg/encounters/vera-molnar/
+    let vera = new paper.Raster("/vera.png");
+    vera.opacity = 0;
+    vera.onLoad = () => {
+        vera.fitBounds(paper.view.bounds);
+
+        let width = paper.view.bounds.width/ resolution;
+        let height = paper.view.bounds.height/ resolution;
+        for (let i = 0; i < resolution; i++) {
+            let x = width * i;
+            for (let j = 0; j < resolution; j++) {
+                let y = height * j;
+                let point = new paper.Point(x, y);
+                let color = vera.getAverageColor(point);
+                let rectangle = new paper.Path.Rectangle(point, new paper.Size(width, height));
+                rectangle.fillColor = color.gray;
+            }
+        }
+    }
+
+
+
+
+
+}
